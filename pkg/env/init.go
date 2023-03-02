8fe1cddbc80a148b7308ac00e7608dbfb100a1a3
mahiro72 <mahiro.yoshie@gmail.com>
2023-03-02 17:26:45 +0900

    mock

--- pkg/env/init.go
+++ pkg/env/init.go
@@ -8,6 +8,10 @@ func init() {
 			fmt.Sprintf("error: config.initEnvironmentConfig failed: %v", err),
 		)
 	}
+	// mock環境の場合mysqlの環境変数設定を行わない
+	if CONFIG.ENVIRONMENT == "mock" {
+		return
+	}
 	if err := initMYSQLConfig(); err != nil {
 		panic(
 			fmt.Sprintf("error: config.initMYSQLConfig failed: %v", err),
