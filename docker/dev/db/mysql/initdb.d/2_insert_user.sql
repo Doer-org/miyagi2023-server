c2f16c6cf5d838ef47f59d0ecc8cce63a19118d1
mahiro72 <mahiro.yoshie@gmail.com>
2023-03-03 15:00:23 +0900

    fix: uuid

--- docker/dev/db/mysql/initdb.d/2_insert_user.sql
+++ docker/dev/db/mysql/initdb.d/2_insert_user.sql
@@ -1,14 +1,14 @@
 INSERT INTO users (id,name,age,gender,birthday,address,profile_img,prefecture) 
-VALUES ("uuid1","hoge1",22,"MEN","2000-04-29","hoge","hoge.com","TOYAMA");
+VALUES ("id1","hoge1",22,"MEN","2000-04-29","hoge","hoge.com","TOYAMA");
 
 INSERT INTO users (id,name,age,gender,birthday,address,profile_img,prefecture) 
-VALUES ("uuid2","hoge2",22,"MEN","2000-04-29","hoge","hoge.com","TOYAMA");
+VALUES ("id2","hoge2",22,"MEN","2000-04-29","hoge","hoge.com","TOYAMA");
 
 INSERT INTO users (id,name,age,gender,birthday,address,profile_img,prefecture) 
-VALUES ("uuid3","hoge3",23,"MEN","2000-04-29","hoge","hoge.com","TOYAMA");
+VALUES ("id3","hoge3",23,"MEN","2000-04-29","hoge","hoge.com","TOYAMA");
 
 INSERT INTO users (id,name,age,gender,birthday,address,profile_img,prefecture) 
-VALUES ("uuid4","hoge4",24,"MEN","2000-04-29","hoge","hoge.com","TOYAMA");
+VALUES ("id4","hoge4",24,"MEN","2000-04-29","hoge","hoge.com","TOYAMA");
 
 INSERT INTO users (id,name,age,gender,birthday,address,profile_img,prefecture) 
-VALUES ("uuid5","hoge5",24,"WOMEN","2000-04-29","hoge","hoge.com","TOYAMA");
+VALUES ("id5","hoge5",24,"WOMEN","2000-04-29","hoge","hoge.com","TOYAMA");
