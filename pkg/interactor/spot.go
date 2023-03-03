245be799a5a2fd7ed46c9090c5cea779f2ef1303
mahiro72 <mahiro.yoshie@gmail.com>
2023-03-03 21:32:22 +0900

    fix:

--- pkg/interactor/spot.go
+++ pkg/interactor/spot.go
@@ -2,12 +2,10 @@ package interactor
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/Doer-org/miyagi2023-server/pkg/domain/model"
 	"github.com/Doer-org/miyagi2023-server/pkg/domain/repository"
+	"github.com/Doer-org/miyagi2023-server/pkg/interactor/mock"
 	"github.com/Doer-org/miyagi2023-server/pkg/usecase"
-	"github.com/google/uuid"
 )
 
 type Spot struct {
@@ -19,76 +17,13 @@ func NewSpot(repository repository.Spot) *Spot {
 }
 
 func (uc *Spot) Get(ctx context.Context, in *usecase.SpotGetInput) (*usecase.SpotGetOutput, error) {
-	id, err := uuid.Parse(in.ID)
-	if err != nil {
-		return nil, err
-	}
-
-	spot, err := uc.repository.Get(ctx, id)
-	if err != nil {
-		return nil, err
-	}
-
-	return &usecase.SpotGetOutput{
-		Spot: spot,
-	}, nil
+	return mock.NewSpotGetOutput(),nil
 }
 
 func (uc *Spot) Create(ctx context.Context, in *usecase.SpotCreateInput) (*usecase.SpotCreateOutput, error) {
-	if in.Name == "" {
-		return nil, fmt.Errorf("spot name is invalid")
-	}
-	if len(in.Name) > 255 {
-		return nil, fmt.Errorf("spot name must be 255 characters or less")
-	}
-
-	if in.Detail == "" {
-		return nil, fmt.Errorf("spot detail is invalid")
-	}
-	if len(in.Detail) > 255 {
-		return nil, fmt.Errorf("spot detail must be 255 characters or less")
-	}
-
-	if in.ImgURL == "" {
-		return nil, fmt.Errorf("spot img_url is invalid")
-	}
-	if len(in.Detail) > 255 {
-		return nil, fmt.Errorf("spot img_url must be 255 characters or less")
-	}
-
-	if in.Address == "" {
-		return nil, fmt.Errorf("spot address is invalid")
-	}
-	if len(in.Address) > 255 {
-		return nil, fmt.Errorf("spot address must be 255 characters or less")
-	}
-
-	s := &model.Spot{
-		ID:      uuid.New(),
-		Name:    in.Name,
-		Detail:  in.Detail,
-		Like:    0,
-		ImgURL:  in.ImgURL,
-		Address: in.Address,
-	}
-
-	newSpot, err := uc.repository.Create(ctx, s)
-	if err != nil {
-		return nil, err
-	}
-
-	return &usecase.SpotCreateOutput{
-		Spot: newSpot,
-	}, nil
+	return mock.NewSpotCreateOutput(),nil
 }
 
 func (uc *Spot) List(ctx context.Context, in *usecase.SpotListInput) (*usecase.SpotListOutput, error) {
-	spots, err := uc.repository.List(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	return &usecase.SpotListOutput{
-		Spots: spots,
-	}, nil
+	return mock.NewSpotListOutput(),nil
 }
--- pkg/interactor/user.go
+++ pkg/interactor/user.go
@@ -2,11 +2,9 @@ package interactor
 
 import (
 	"context"
-	"fmt"
-	"time"
 
-	"github.com/Doer-org/miyagi2023-server/pkg/domain/model"
 	"github.com/Doer-org/miyagi2023-server/pkg/domain/repository"
+	"github.com/Doer-org/miyagi2023-server/pkg/interactor/mock"
 	"github.com/Doer-org/miyagi2023-server/pkg/usecase"
 )
 
@@ -19,86 +17,9 @@ func NewUser(repository repository.User) *User {
 }
 
 func (uc *User) Get(ctx context.Context, in *usecase.UserGetInput) (*usecase.UserGetOutput, error) {
-	if in.ID == "" {
-		return nil, fmt.Errorf("user id is invalid")
-	}
-
-	user, err := uc.repository.Get(ctx, model.UserID(in.ID))
-	if err != nil {
-		return nil, err
-	}
-
-	return &usecase.UserGetOutput{
-		User: user,
-	}, nil
+	return mock.NewUserGetOutput(),nil
 }
 
 func (uc *User) Create(ctx context.Context, in *usecase.UserCreateInput) (*usecase.UserCreateOutput, error) {
-	if in.ID == "" {
-		return nil, fmt.Errorf("user id is invalid")
-	}
-	if len(in.ID) > 255 {
-		return nil, fmt.Errorf("user id must be 255 characters or less")
-	}
-
-	found, _ := uc.repository.Get(ctx, model.UserID(in.ID))
-	if found != nil {
-		return nil, fmt.Errorf("user id is exists")
-	}
-
-	if in.Name == "" {
-		return nil, fmt.Errorf("user name is invalid")
-	}
-	if len(in.Name) > 255 {
-		return nil, fmt.Errorf("user name must be 255 characters or less")
-	}
-
-	if in.Age == 0 {
-		return nil, fmt.Errorf("user age is invalid")
-	}
-	if in.Age > 100 {
-		return nil, fmt.Errorf("user age max is 100")
-	}
-
-	var gender model.Gender
-	switch in.Gender {
-	case string(model.MEN):
-		gender = model.Gender(in.Gender)
-	case string(model.WOMEN):
-		gender = model.Gender(in.Gender)
-	default:
-		return nil, fmt.Errorf("user gender is invalid")
-	}
-
-	birthday, err := time.Parse("2006-01-02", in.Birthday)
-	if err != nil {
-		return nil, err
-	}
-
-	if in.Address == "" {
-		return nil, fmt.Errorf("user address is invalid")
-	}
-	if len(in.Address) > 255 {
-		return nil, fmt.Errorf("user address must be 255 characters or less")
-	}
-
-	u := &model.User{
-		ID:         model.UserID(in.ID),
-		Name:       in.Name,
-		Age:        in.Age,
-		Gender:     gender,
-		Birthday:   birthday,
-		Address:    in.Address,
-		ImgURL: in.ImgURL,                   //必須ではない
-		Prefecture: model.Prefecture(in.Prefecture), //FIXME: 必須?
-	}
-
-	newUser, err := uc.repository.Create(ctx, u)
-	if err != nil {
-		return nil, err
-	}
-
-	return &usecase.UserCreateOutput{
-		User: newUser,
-	}, nil
+	return mock.NewUserCreateOutput(),nil
 }
